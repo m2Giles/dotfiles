7ac80234256c6af8bb26ffbb0add0def7be4ed63
Mark Mihalik <markamihalik@gmail.com>
2023-09-19 17:11:56 -0400

    fix: Fix tab complete for ultisnips/luasnip

--- nvim/dot-config/nvim/lua/plugins/ultisnips.lua
+++ nvim/dot-config/nvim/lua/plugins/ultisnips.lua
@@ -13,12 +13,6 @@ return {
       "SirVer/ultisnips",
     },
     opts = function(_, opts)
-      local has_words_before = function()
-        unpack = unpack or table.unpack
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-      end
-
       local cmp = require("cmp")
       local luasnip = require("luasnip")
       local ultisnip = require("cmp_nvim_ultisnips.mappings")
@@ -31,17 +25,11 @@ return {
       opts.mapping = vim.tbl_extend("force", opts.mapping, {
         ["<Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
-            cmp.select_next_item()
-            -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-            -- this way you will only jump inside the snippet region
+            cmp.confirm({ select = true })
           elseif luasnip.expand_or_jumpable() then
             luasnip.expand_or_jump()
-          elseif ultisnip.expand_or_jump_forwards() then
-            ultisnip.expand_or_jump_forwards()
-          elseif has_words_before() then
-            cmp.complete()
           else
-            fallback()
+            ultisnip.expand_or_jump_forwards(fallback)
           end
         end, { "i", "s" }),
         ["<S-Tab>"] = cmp.mapping(function(fallback)
@@ -49,10 +37,8 @@ return {
             cmp.select_prev_item()
           elseif luasnip.jumpable(-1) then
             luasnip.jump(-1)
-          elseif ultisnip.jump_backwards() then
-            ultisnip.jump_backwards()
           else
-            fallback()
+            ultisnip.jump_backwards(fallback)
           end
         end, { "i", "s" }),
       })
